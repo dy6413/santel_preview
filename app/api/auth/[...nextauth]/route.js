02d58da7b5f171d8d6a5450cffbb3c6e5d8bd673
yun <dyyun00@gmail.com>
2025-12-11 16:30:01 +0900

    updaf

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { authOptions } from "../../../../lib/authOptions";
+import { authOptions } from "../../../lib/authOptions";
 
 export const GET = NextAuth(authOptions);
 export const POST = NextAuth(authOptions);

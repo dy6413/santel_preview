0e37cc0d086e50235e561680c7c66e2b2cfc2ca3
yun <dyyun00@gmail.com>
2025-12-12 16:17:16 +0900

    qtgpt

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    appDir: true,
+  },
+} as any;
 
 export default nextConfig;
+
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
